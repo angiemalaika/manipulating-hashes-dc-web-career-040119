8694f241e996f06f0adb5e08b181f498ce569775
Angela Mumba <malaika.mumba@gmail.com>
2019-03-30 05:15:33 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,8 +7,7 @@ def second_challenge
   }
 
   #code your solution here!
-groceries.each do |food, data|
-    data.each do |attribute, value|
+groceries.values
        value.values 
       end 
     end 
